41663701b10f3b494b2e9dbabecd12bceba83e2c
ww8007 <wshmin@naver.com>
2020-12-09 23:40:30 +0900

    인덱스연산-주의

---
+++ Study/Index/index연산.py
@@ -0,0 +1,9 @@
+man = ["스파", "베트", "헐크", "캣"]    
+a = man.index("스파")
+print(a)
+
+#아래와 위의 차이점 index.index로 함수를 불러온 것 임으로 혼동하지 말 것!!
+
+how = ["스파", "베트", "헐크", "캣"]    
+index = man.index("헐크")
+print(index)
