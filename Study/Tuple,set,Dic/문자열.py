45d246ff115397312bb3e065a33f721d33175a85
ww8007 <wshmin@naver.com>
2020-12-10 03:27:24 +0900

    문자열code

---
+++ Study/Tuple,set,Dic/문자열.py
@@ -0,0 +1,24 @@
+word = "Python"
+#문자열은 슬라이싱 가능
+b = word[0:2]
+print(type(word))
+print(type(b))
+
+print(word)
+print(b)
+
+
+w1="a"
+w2="baba"
+w3="aabbcc"
+w4="aaaaaaaa"
+
+if (w1 < w4):
+    print("a가 앞에")
+else:
+    print("b가 앞에")
+
+if (w1 < w2):
+    print("a가 앞에")
+else:
+    print("b가 앞에")
