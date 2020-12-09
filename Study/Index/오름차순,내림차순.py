4f18b65f4fa25cb2f6dab3e5d2f10f163c8339fc
ww8007 <wshmin@naver.com>
2020-12-10 00:44:09 +0900

    sort-혼동쉬움

---
+++ Study/Index/오름차순,내림차순.py
@@ -0,0 +1,14 @@
+heroes = ["아이언맨", "토르", "헐크", "나다", "나나", "가", "스칼렛 위치", "아이언 맨"]
+#흔히 정렬을 생각하면 길이로 착각하는데 이것이 아닌 사전적 의미가 맨 처음이고 그 다음이 길이로 들어간다.
+print(heroes)
+heroes.sort()
+print(heroes)
+heroes.sort(reverse=True)
+#reverse는 
+print(heroes)  
+
+my = [5, 1, 3, 2, 4]
+print(my)
+my.sort()
+print(my)
+
