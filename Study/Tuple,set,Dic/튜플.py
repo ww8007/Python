73816919bd0b2ccc5aca33c016ab5cbe58e19881
ww8007 <wshmin@naver.com>
2020-12-10 01:08:58 +0900

    튜플정리코드

---
+++ Study/Tuple,set,Dic/튜플.py
@@ -0,0 +1,13 @@
+col = ("red", "blue", "green")
+print(col)
+
+numbers=(1,2,3, "사")
+
+print(numbers)
+
+# 리스트 튜플 변환 -> 반환의 개념으로 생각
+my = [1, 2, 3, 4, 5]
+print(type(my))
+a = tuple(my)
+print(type(a))
+
